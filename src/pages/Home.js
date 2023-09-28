929277a225314929f9806fac8d7accd343273202
Lenny Mwangi <mwangileny8@gmail.com>
2023-09-28 10:47:11 +0300

    updated main banner

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="home-wrapper-1 py-5">
+      <div className="container-xxl">
+        <div className="col-6">
+          <div className="main-banner-cntent p-3">
+            <img src="" className="img-fluid rounded-3" alt="main banner"/>
+            <div className="main-banner-content">
+              <h4></h4>
+              <h5></h5>
+              <p></p>
+              <Link></Link>
+            </div>
+          </div>
+        </div>
+        <div className="col-6"></div>
+      </div>
+    </section>>
   )
 }
 
